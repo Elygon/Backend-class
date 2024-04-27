ad59e07173655c565e0ce654022929289eac27ab
Elygon <elygondean@gmail.com>
2024-04-27 15:37:24 +0100

    first commit

--- index.js
+++ index.js
@@ -6,3 +6,15 @@ console.log(z);
 
 let name = "John Mote", age = 100, salary = 10000000000;
 console.log(age);
+
+//Classwork
+let a = 5;
+let b = 10;
+let c = a * b;
+let d = c/2;
+console.log(d);
+
+let sigma = "Elygon";
+let friend = " and Timothy";
+let clique = sigma + friend;
+console.log(clique);
